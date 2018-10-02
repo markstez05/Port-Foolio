9200a83fb3e68bd449236b9c3040ffe5fae67e3e
Your Name <you@example.com>
2018-10-02 16:35:06 -0500

    arcade layout

---
+++ src/components/main.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import "./main.css"
+
+
+class Main extends Component {
+    constructor(props) {
+      super(props);
+    }
+
+render () {
+    return (
+        <div className="main">
+        {/* <div><span></span></div> */}
+        <div className="container">
+        <div className="Nav">
+        <div className="left-box"></div>
+        <div className="nav-name">
+        
+            <p className="nav-name">M.A.R.K. ST3Z</p>      
+            </div>
+            <div className="right-box"></div>
+        </div>
+        <div className="shadow-box-div">
+        <div className="shadow-box1"></div>
+        <div className="middle-box"></div>
+        <div className="shadow-box"></div>
+        </div>
+        <div className="screen1">
+        <div className="screen">
+        <div></div>
+        </div>
+        </div>
+        <div className="controller-box">
+        <div className="controller-box-left"></div>
+        <div className="controller-box-mid"> 
+        <div className="joystick">
+        </div>
+        <div className="buttons">
+        <div className="button1"></div><div className="button2"></div>
+        <div className="button1a"></div><div className="button2"></div>
+        </div>
+        </div>
+        <div className="controller-box-right"></div>
+        </div>
+       
+       <div className="coin"></div>
+        </div>
+        </div>
+        )
+    }
+};
+
+export default Main 
