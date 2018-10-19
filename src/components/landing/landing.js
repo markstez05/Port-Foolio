b70f493aed4e8f0bc93747745217861a1eed3a48
Your Name <mstesney12@gmail.com>
2018-10-19 14:11:26 -0500

    more... CH CH CH CHANGES!

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -11,27 +11,27 @@ import git from "../../images/git.png";
 class Landing extends Component {
     hover(){
         document.getElementById("profile").onmouseover = function(){
-        document.getElementById('stats').style.width = '55%';
-        document.getElementById("stats").style.transitionDelay = "1.7s";
         document.getElementById("profile").style.transitionDelay = "0s";
+        document.getElementById('stats').style.width = '55%';
+        document.getElementById("stats").style.transitionDelay = "1.3s";
         document.getElementById("info").style.opacity = "1";
-        document.getElementById("info").style.transition = " all ease-in 1.1s";
-        document.getElementById("info").style.transitionDelay = "2s";
+        document.getElementById("info").style.transition = " all ease-in 1.3s";
+        document.getElementById("info").style.transitionDelay = "1.4s";
         document.getElementById("logo").style.opacity = "1";
-        document.getElementById("logo").style.transition = "all ease-in 1.1s";
-        document.getElementById("logo").style.transitionDelay = "3.5s";
+        document.getElementById("logo").style.transition = "all ease-in 1.3s";
+        document.getElementById("logo").style.transitionDelay = "3s";
     }
   }
    hoverOff(ev){ document.getElementById('profile').onmouseout = function(){
-        document.getElementById("stats").style.transition = "all 1.8s";
-        document.getElementById("stats").style.transitionDelay = "0s";
-        document.getElementById("profile").style.transition = "all 1.8s";
-        document.getElementById("profile").style.transitionDelay = "1.9s";
+        document.getElementById("stats").style.transition = "all ease-out 1.2s";
+        document.getElementById("stats").style.transitionDelay = "1s";
         document.getElementById('stats').style.width = "";
+        document.getElementById("profile").style.transition = "all ease-out 1.3s";
+        document.getElementById("profile").style.transitionDelay = "2.4s";
         document.getElementById("info").style.opacity = "";
-        document.getElementById("info").style.transitionDelay = "-.45s"
+        document.getElementById("info").style.transitionDelay = ".32s"
         document.getElementById("logo").style.opacity = "";
-        document.getElementById("logo").style.transitionDelay = "-.45s"
+        document.getElementById("logo").style.transitionDelay = "-.5s"
     }
 };
   render() {
@@ -44,6 +44,15 @@ class Landing extends Component {
             <div className="skew"></div>
             <h3 className="name">About Me</h3>
             <h5 className="title">Full Stack Web Developer</h5>
+            <div className="profile-stuff">
+            <h5 className="stuff-info">Fayetteville, AR</h5>
+            <a className="email"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="mailto:mstesney12@gmail.com">
+            mstesney12@gmail.com</a>
+            <h5 className="stuff-info">(918)361-8479</h5>
+            </div>
             <div className="icon-div">
             <a
               target="_blank"
@@ -99,6 +108,12 @@ class Landing extends Component {
             <div className="tech-box">
             <img src={ mongo } alt="tech" className="tech"/>
             <h5 className="tech-title">MongoDB</h5>
+            </div>
+            </div>
+            <h4 className="info-title2"></h4>
+            <div className="tech-container">
+            <div className="tech-box">
+              <h5 className="bio">My skills include Full Stack Web Development as well as supervising and facilitating multi-faceted projects, troubleshooting an array of technological issues, managing and developing groups of people, orchestrating positive client experiences, and engaging in constructive analysis feedback.</h5>
             </div>
             </div>
             </div>
