59e7e330992b841ee77ff2cf4056e8d8118ecb02
Your Name <mstesney12@gmail.com>
2018-11-14 15:46:18 -0600

    asfd

---
+++ src/components/resume/resume.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import "./main.css"
+
+
+class Resume extends Component {
+
+
+render () {
+    return (
+        <div id="resume" className="resume">
+        <div className="resume-container">
+        
+        </div>
+        </div>
+        )
+    }
+};
+
+export default Resume
